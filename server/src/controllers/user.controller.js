047e87eb7d8feba8f84cb8b526cc27cbb0a5a740
harsha12a <harsha.pss2005@gmail.com>
2025-04-08 17:44:45 +0530

    user crud

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,11 +1,13 @@
 import prisma from "../utils/prisma.config.js"
+import asyncHandler from "express-async-handler"
+import bcrypt from "bcrypt"
 
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = asyncHandler ( async (req, res) => {
     let resp = await prisma.user.findMany()
     res.send(resp)
-}
+})
 
-export const getUserById = async (req, res) => {
+export const getUserById = asyncHandler ( async (req, res) => {
     let resp = await prisma.user.findUnique({
         where: {
             id: req.params.id
@@ -13,4 +15,41 @@ export const getUserById = async (req, res) => {
     })
     if (!resp) return res.status(404).send({ message: "User not found" })
     else res.send(resp)
-}
+})
+
+export const createUser = asyncHandler ( async (req, res) => {
+    try {
+        let resp = await prisma.user.create({
+            data: {
+                username: req.body.username.toLowerCase(),
+                email: req.body.email.toLowerCase(),
+                password: await bcrypt.hash(req.body.password, 10),
+                profilePic: req.body.profilePic
+            }
+        })
+        res.send({ message: "Created", payload: resp })
+    }
+    catch (error) {
+        if (error.code === "P2002") return res.status(400).send({ message: "Email already exists" })
+        else return res.status(500).send({ message: error.message })
+    }
+})
+
+export const loginUser = asyncHandler ( async (req, res) => {
+    try {
+        let resp = await prisma.user.findUnique({
+            where: {
+                username: req.body.username.toLowerCase()
+            }
+        })
+        if (!resp) return res.status(404).send({ message: "User not found" })
+        else {
+            const isMatch = await bcrypt.compare(req.body.password, resp.password)
+            if (!isMatch) return res.status(401).send({ message: "Invalid credentials" })
+            else res.send({ message: "Login successful", resp })
+        }
+    }
+    catch (error) {
+        return res.status(500).send({ message: error.message })
+    }
+})
--- server/src/routers/user.route.js
+++ server/src/routers/user.route.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { getAllUsers, getUserById } from "../controllers/user.controller.js"
+import { getAllUsers, getUserById, createUser, loginUser } from "../controllers/user.controller.js"
 const router = express.Router()
 
 router.get('/', getAllUsers)
 router.get('/:id', getUserById)
+router.post('/', createUser)
+router.post('/login', loginUser)
 
 export default router
